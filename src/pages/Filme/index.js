44a3ba43d5dae3c591b09c57a090e9ec72dc8976
Emerson Xavier <meso.xavier@gmail.com>
2023-11-10 10:50:30 -0300

    att commit

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api';
 import './style.css';
+import { toast } from 'react-toastify'
 
 function Filme(){
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -22,6 +24,8 @@ function Filme(){
             })
             .catch(() => {
                 console.log("Filme não encontrado")
+                navigate("/", { replace: true })
+                return;
             })
         }
 
@@ -30,7 +34,24 @@ function Filme(){
         return () => {
             console.log("COMPONENTE FOI DESMONTADO")
         }
-    }, [])
+    }, [navigate, id])
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some( (filmesSalvos) => filmesSalvos.id === filme.id)
+
+        if(hasFilme){
+            toast.warn("Esse filme já está na sua lista");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com sucesso!");
+    }
 
     if(loading){
         return(
@@ -50,9 +71,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
